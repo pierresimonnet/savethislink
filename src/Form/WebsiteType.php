9d6d8f0eef6aa1bbc0a5a7e7be2421673abbaea3
Pierre Simonnet <pierresimonnet@protonmail.com>
2021-04-18 20:17:49 +0200

    ressource: website entity and symfony form

---
+++ src/Form/WebsiteType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Website;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class WebsiteType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('url', UrlType::class)
+            ->add('title', TextType::class)
+            ->add('description', TextareaType::class)
+            ->add('comment', TextareaType::class)
+            ->add('submit', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Website::class,
+        ]);
+    }
+}
