1df1785213e0d310772ea1ee5cc72103bed644e7
Pierre Simonnet <pierresimonnet@protonmail.com>
2021-04-25 13:29:10 +0200

    api/ressource : Theme, make base crud

--- src/Entity/Theme.php
+++ src/Entity/Theme.php
@@ -7,6 +7,7 @@ use App\Repository\ThemeRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 /**
  * @ApiResource()
@@ -18,11 +19,13 @@ class Theme
      * @ORM\Id
      * @ORM\GeneratedValue
      * @ORM\Column(type="integer")
+     * @Groups({"website:read"})
      */
     private $id;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Groups({"website:read"})
      */
     private $title;
 
--- src/Form/ThemeType.php
+++ src/Form/ThemeType.php
@@ -2,27 +2,27 @@
 
 namespace App\Form;
 
-use App\Entity\Website;
+use App\Entity\Theme;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class WebsiteType extends AbstractType
+class ThemeType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('url', UrlType::class)
-            ->add('comment', TextareaType::class)
+            ->add('title', TextType::class)
+            ->add('description', TextareaType::class)
         ;
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            'data_class' => Website::class,
+            'data_class' => Theme::class,
         ]);
     }
 }
