753e9b6d9ea42aa80d5197a33cd5dcfe9e7582d3
Pierre Simonnet <pierresimonnet@protonmail.com>
2021-05-05 14:43:59 +0200

    front: styling

--- assets/js/items/ItemApp.jsx
+++ assets/js/items/ItemApp.jsx
@@ -74,10 +74,21 @@ const ItemApp = ({ user, ressource, theme = null, owner = null }) => {
 
   return (
     <>
-      <div className="text-center">
+      <div className="d-flex justify-flex-center mb-1">
         {user && (
-          <button onClick={toggle} className="button-primary button-big">
-            Create
+          <button onClick={toggle} className="button-primary">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="24px"
+              viewBox="0 0 24 24"
+              width="24px"
+              fill="#ffffff"
+              id="add"
+            >
+              <path d="M0 0h24v24H0z" fill="none" />
+              <path d="M13 7h-2v4H7v2h4v4h2v-4h4v-2h-4V7zm-1-5C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z" />
+            </svg>
+            Add
           </button>
         )}
       </div>
