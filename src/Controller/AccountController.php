3c777b91da69950305fd599ab25bf83efa47ac9f
Pierre Simonnet <pierresimonnet@protonmail.com>
2021-05-08 14:58:20 +0200

    back: fix delete account

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -5,13 +5,25 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Form\UserType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Security\Http\Event\LogoutEvent;
 
 #[Route('/account')]
 class AccountController extends AbstractController
 {
+    private EventDispatcherInterface $eventDispatcher;
+    private TokenStorageInterface $tokenStorage;
+
+    public function __construct(EventDispatcherInterface $eventDispatcher, TokenStorageInterface $tokenStorage)
+    {
+        $this->eventDispatcher = $eventDispatcher;
+        $this->tokenStorage = $tokenStorage;
+    }
+
     #[Route('/edit/{id}', name: 'account_edit', methods: ['GET', 'POST'])]
     public function profile(Request $request, User $user): Response
     {        
@@ -39,9 +51,14 @@ class AccountController extends AbstractController
         $this->denyAccessUnlessGranted('ROLE_USER');
 
         if ($this->isCsrfTokenValid('delete'.$user->getId(), $request->request->get('_token'))) {
+            $this->eventDispatcher->dispatch(new LogoutEvent($request, $this->tokenStorage->getToken()));
+            $this->tokenStorage->setToken(null);
+
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->remove($user);
             $entityManager->flush();
+
+            $this->addFlash('success', 'Account deleted');
         }
 
         return $this->redirectToRoute('app_home');
