f74bdc13b5b3767abfb523e69d6b7af8decdd687
Pierre Simonnet <pierresimonnet@protonmail.com>
2021-05-20 16:57:42 +0200

    feature: removed dark mode (to be implemented in v2)

--- assets/js/elements/index.js
+++ assets/js/elements/index.js
@@ -4,4 +4,4 @@ import { ThemeSwitcher } from "./ThemeSwitcher";
 window.customElements.define("input-switch", Switch, {
   extends: "input",
 });
-window.customElements.define("theme-switcher", ThemeSwitcher);
+window.customElements.define("theme-switcher", ThemeSwitcher); // <theme-switcher></theme-switcher>
