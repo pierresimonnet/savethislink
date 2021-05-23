cf4fc71870cc34b33ad21dc51ec4631f741d843a
Pierre Simonnet <pierresimonnet@protonmail.com>
2021-05-23 15:51:41 +0200

    back: fix topic voter

--- src/Security/Voter/ThemeVoter.php
+++ src/Security/Voter/ThemeVoter.php
@@ -19,10 +19,6 @@ class ThemeVoter extends Voter
     protected function voteOnAttribute($attribute, $subject, TokenInterface $token)
     {
         $user = $token->getUser();
-        // if the user is anonymous, do not grant access
-        if (!$user instanceof UserInterface) {
-            return false;
-        }
 
         /** @var \App\Entity\Theme $subject */
         switch ($attribute) {
