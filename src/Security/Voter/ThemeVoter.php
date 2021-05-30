bd8029332236301c3abe8fbdbff4a786989b8918
Pierre Simonnet <pierresimonnet@protonmail.com>
2021-05-30 14:16:01 +0200

    security: god mode enable

--- src/Security/Voter/ThemeVoter.php
+++ src/Security/Voter/ThemeVoter.php
@@ -20,6 +20,10 @@ class ThemeVoter extends Voter
     {
         $user = $token->getUser();
 
+        if ($this->security->isGranted('ROLE_SUPER_ADMIN')) {
+            return true;
+        }
+
         /** @var \App\Entity\Theme $subject */
         switch ($attribute) {
             case 'TOPIC_CREATE_CONTENT':
